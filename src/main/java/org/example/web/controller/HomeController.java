1bb4de8577982bd5486a870f1531c33ce6ddf8bb
Manish Baxi <baxi.manish@welue.com>
2014-02-16 16:27:31 +0530

    Added Google App Engine configuration file.

--- src/main/java/org/example/web/controller/HomeController.java
+++ src/main/java/org/example/web/controller/HomeController.java
@@ -3,15 +3,14 @@ package org.example.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController
 {
-    @RequestMapping(method = RequestMethod.GET)
-    public String show()
-    {
-        return "home";
-    }
+  @RequestMapping(method = RequestMethod.GET)
+  public String show()
+  {
+    return "home";
+  }
 }
